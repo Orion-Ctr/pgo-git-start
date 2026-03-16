3c9afd92fdb123dd7ae2d41cae84cac189d290cb
Orion-Ctr <salmanaminu0001@gmail.com>
2026-03-16 23:48:09 +0100

    Final Main File for 5 Mark

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello PGO!");
         System.out.println("The is exercise 1 with git ");
-        System.out.println("This will be very useful for working in teamstatus");
+        System.out.println("This will be very useful for working in teams");
+        // I should probably use a different method to print all this
     }
 }
